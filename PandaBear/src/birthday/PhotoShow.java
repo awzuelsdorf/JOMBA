25d5ae08905e75d02141a2d66464f1b9418ac9fa
Andrew Zuelsdorf <andrezu1@umbc.edu>
2015-07-02 20:37:39 -0400

    First (birthday) version of Photo Synthesis

--- PandaBear/src/birthday/PhotoShow.java
+++ PandaBear/src/birthday/PhotoShow.java
@@ -1,8 +1,8 @@
-/*	This file is part of Photo Synthesis.
+/*	This file is part of PhotoShow.
 
 	Copyright 2015 Andrew Zuelsdorf. Licensed under GNU GPL
 
-    Photo Synthesis is free software: you can redistribute it and/or modify
+    PhotoShow is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
