515566f3607d1fad0413b915668a3f6771be6aa6
Your Name <you@example.com>
2015-07-04 20:21:18 -0400

    Getting rid of cruft

--- DiscrepancyException.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class DiscrepancyException extends RuntimeException{
-	private static final long serialVersionUID = -2920670722114622878L;
-	public DiscrepancyException(String message) {
-		super(message);
-	}
-}
